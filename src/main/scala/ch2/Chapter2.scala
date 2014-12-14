b120cc594080a66f0f39a1700fa8a0f33948fe70
fede <fede.lopez@gmail.com>
2014-12-14 21:05:08 +1100

    Up to exercise 2.4

--- src/main/scala/ch2/Chapter2.scala
+++ src/main/scala/ch2/Chapter2.scala
@@ -34,4 +34,9 @@ object Chapter2 {
    */
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
 
+  /**
+   * Exercise 2.4
+   */
+  def uncurry[A, B, C](f: A => (B => C)): (A, B) => C = (a: A, b: B) => f(a)(b)
+
 }
