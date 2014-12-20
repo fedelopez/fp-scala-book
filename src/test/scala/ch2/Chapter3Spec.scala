7217111d867252155ee93f0768166d34d3a3a053
fede <fede.lopez@gmail.com>
2014-12-20 23:43:58 +1100

    Renamed

--- src/test/scala/ch2/Chapter3Spec.scala
+++ src/test/scala/ch2/Chapter3Spec.scala
@@ -7,31 +7,31 @@ import org.scalatest.{FlatSpec, Matchers}
  */
 class Chapter3Spec extends FlatSpec with Matchers {
 
-  it should "be 3 the result in exercise31" in {
+  it should "3.1 be 3 the result" in {
     Chapter3.exercise31() should be(3)
   }
 
-  it should "3.2: should remove the first element in the list" in {
+  it should "3.2 remove the first element in the list" in {
     val tail: Chapter3.List[Int] = Chapter3.List.tail(Chapter3.List(1, 2, 3, 4))
     tail should equal(Chapter3.List(2, 3, 4))
   }
 
-  it should "3.2: should return empty list when no tail" in {
+  it should "3.2 return empty list when no tail" in {
     val tail: Chapter3.List[Int] = Chapter3.List.tail(Chapter3.List())
     tail should equal(Chapter3.List())
   }
 
-  it should "3.3: should set the first element in the list" in {
+  it should "3.3 set the first element in the list" in {
     val tail: Chapter3.List[Int] = Chapter3.List.setHead(Chapter3.List(1, 2, 3, 4), 6)
     tail should equal(Chapter3.List(6, 2, 3, 4))
   }
 
-  it should "3.3: should set the first element in the empty list" in {
+  it should "3.3 set the first element in the empty list" in {
     val result: Chapter3.List[Int] = Chapter3.List.setHead(Chapter3.List(), 1)
     result should equal(Chapter3.List(1))
   }
 
-  it should "3.3: should set the first element in the singleton list" in {
+  it should "3.3 set the first element in the singleton list" in {
     val result: Chapter3.List[Int] = Chapter3.List.setHead(Chapter3.List(1), 2)
     result should equal(Chapter3.List(2))
   }
