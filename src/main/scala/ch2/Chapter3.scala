5acaff1525acabde920d0f9cf5003c9b2f03539d
fede <fede.lopez@gmail.com>
2014-12-20 17:09:08 +1100

    Up to exercise 3.4

--- src/main/scala/ch2/Chapter3.scala
+++ src/main/scala/ch2/Chapter3.scala
@@ -20,6 +20,24 @@ object Chapter3 {
     def apply[A](as: A*): List[A] =
       if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
+
+    /**
+     * exercise 3.2
+     * Implement the function tail for removing the first element of the list.
+     */
+    def tail[A](x: List[A]): List[A] = x match {
+      case Nil => Nil
+      case Cons(head, tail) => tail
+    }
+
+    /**
+     * exercise 3.3
+     * Implement the function setHead for replacing the first element of the list.
+     */
+    def setHead[A](x: List[A], newHead: A): List[A] = x match {
+      case Nil => Cons(newHead, Nil)
+      case Cons(xs, y) => Cons(newHead, tail(x))
+    }
   }
 
   /**
--- src/test/scala/ch2/Chapter3Spec.scala
+++ src/test/scala/ch2/Chapter3Spec.scala
@@ -10,4 +10,29 @@ class Chapter3Spec extends FlatSpec with Matchers {
   it should "be 3 the result in exercise31" in {
     Chapter3.exercise31() should be(3)
   }
+
+  it should "3.2: should remove the first element in the list" in {
+    val tail: Chapter3.List[Int] = Chapter3.List.tail(Chapter3.List(1, 2, 3, 4))
+    tail should equal(Chapter3.List(2, 3, 4))
+  }
+
+  it should "3.2: should return empty list when no tail" in {
+    val tail: Chapter3.List[Int] = Chapter3.List.tail(Chapter3.List())
+    tail should equal(Chapter3.List())
+  }
+
+  it should "3.4: should set the first element in the list" in {
+    val tail: Chapter3.List[Int] = Chapter3.List.setHead(Chapter3.List(1, 2, 3, 4), 6)
+    tail should equal(Chapter3.List(6, 2, 3, 4))
+  }
+
+  it should "3.4: should set the first element in the empty list" in {
+    val result: Chapter3.List[Int] = Chapter3.List.setHead(Chapter3.List(), 1)
+    result should equal(Chapter3.List(1))
+  }
+
+  it should "3.4: should set the first element in the singleton list" in {
+    val result: Chapter3.List[Int] = Chapter3.List.setHead(Chapter3.List(1), 2)
+    result should equal(Chapter3.List(2))
+  }
 }
